f283c9b98ef777f9f4fee00972f802886b5a51d8
daruvurisowmya <109509763+daruvurisowmya@users.noreply.github.com>
2022-10-01 13:35:38 +0530

    Add files via upload

---
+++ end_sep_methods.py
@@ -0,0 +1,63 @@
+#(1,2,3,4,5) #1 2 3 4 5
+#print('pyt' 'hon') #python
+#print('pyt','hon') #pyt hon
+#print('pro'+str(3)) #pro3
+#print('Poco' 'M4' 'Pro') #PocoM4Pro
+#print('Poco','M4','Pro') #Poco M4 Pro
+#print('Poco'+''+'M4'+''+'Pro') #PocoM4Pro
+#print('Poco'+' '+'M4'+' '+'Pro') #Poco M4 Pro
+#print('Poco'+' ,'+'M4'+', '+'Pro') #Poco ,M4, Pro
+#print('Poco'+'  '+'M4'+'  '+'Pro') #Poco M4 Pro
+
+#print(1,2,3,4,5) #1 2 3 4 5
+#print('1'+'2'+'3'+'4'+'5') #12345
+#print('1'+''+'2'+''+'3'+''+'4'+''+'5') #12345
+#print('1'+' '+'2'+' '+'3'+' '+'4'+' '+'5') #1 2 3 4 5
+
+#print(1,2,3,4,5,sep='') #12345
+#print('h' 'i',sep=' ') #hi
+#print('h', 'i',sep=' ') #h i
+#print('h', 'i',sep='-') #h-i
+
+#print(10,20,30,40,sep='-') #10-20-30-40
+#print(10,20,30,40,sep='@') #10@20@30@40
+#print(10,20,30,40,sep='*') #10*20*30*40
+
+#print('python',end='')
+#print('program') #pythonprogram
+
+#print('python',end=' ')
+#print('program') #python program
+
+#print('python',end='.')
+#print('program') #python.program
+
+#print('python',end='. ')
+#print('program') #python. program
+#print('python',end='')
+#print('code',end='')
+#print('programmer') #pythoncodeprogrammer
+
+#print('python',end=' ')
+#print('code',end=' ')
+#print('programmer') #python code programmer
+
+#print(1,2,3,4,end=' ',sep='-')
+#print(5,6,7,8,sep=' ',end='.')
+#print(9,10) #1-2-3-4-5 6 7 8.9 10
+
+#print('python\n''program') 
+#print('python''\nprogram')
+#print('python\n''\nprogram')
+
+#print('hello' * 3) #hellohellohello
+#print('hello ' * 3) #hello hello hello  
+#print('hello\n' * 3) #hello 
+                     #hello
+                     #hello
+
+#print('pytho\n'*3,end='.')
+#print('pytho\n'*3,end='')
+
+#print('hello',end='\n') #hello
+#print('code')           #code
